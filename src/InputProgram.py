ad4317196a3c3c87e9221d9e85c6cd8beb49690f
dalma6 <beara.dalma@gmail.com>
2020-04-12 18:00:31 +0200

    add function call recognition for basic blocks division

--- src/InputProgram.py
+++ src/InputProgram.py
@@ -1,4 +1,5 @@
 from src.BasicBlock import BasicBlock
+import re
 
 class InputProgram:
     def __init__(self, code):
@@ -22,9 +23,11 @@ class InputProgram:
     def get_leaders(self, instructions):
         leaders = [instructions[0]]
         control_flow_changers = ['if', 'else', 'elif']
+        function_call = re.compile('\w+\([\w\s\d|,]*\)')
 
         for i in range(len(instructions)):
-             if any(cfc in instructions[i] for cfc in control_flow_changers):
+            function_call_occurs = function_call.findall(instructions[i])
+            if any(cfc in instructions[i] for cfc in control_flow_changers) or 0 != len(function_call_occurs):
                 if i + 1 != len(instructions):
                     leaders.append(instructions[i+1])
 
