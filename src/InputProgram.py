a93d959067a44f5635ed3f65af87a517080e3931
dalma6 <beara.dalma@gmail.com>
2020-04-25 19:48:50 +0200

    fix basic blocks

--- src/InputProgram.py
+++ src/InputProgram.py
@@ -7,23 +7,38 @@ class InputProgram:
         self.basic_blocks = self.divide_into_basic_blocks
 
     def make_instructions(self, code):
-        return code.split('\n')
+        code = code.split('\n')
+        instructions = []
+        for instruction in code:
+            if instruction.strip() != '':
+                instructions.append(instruction)
+        return instructions
 
     def divide_into_basic_blocks(self, instructions):
         leaders = self.get_leaders(instructions)
         basic_blocks = []
 
-        for instruction in instructions:
-            if leaders.__contains__(instruction):
-                basic_blocks.append(BasicBlock(instruction))
-            basic_blocks[-1].add_instruction(instruction)
+        for i in range(len(leaders)-1):
+            current_leader = leaders[i]
+            next_leader = leaders[i+1]
+
+            cl_in_instructions = instructions.index(current_leader) if current_leader in instructions else -1
+            nl_in_instructions = instructions.index(next_leader) if next_leader in instructions else -1
+
+            basic_blocks.append(BasicBlock(leaders))
+            if -1 == cl_in_instructions:
+                return basic_blocks
+
+            for instruction in instructions[cl_in_instructions : nl_in_instructions]:
+                basic_blocks[-1].add_instruction(instruction)
+
+            instructions = instructions[nl_in_instructions : -1]
 
         return basic_blocks
 
     def get_leaders(self, instructions):
         leaders = [instructions[0]]
         control_flow_changers = ['if', 'else', 'elif']
-
 
         for i in range(len(instructions)):
             function_call_occurs = len(re.findall('^(?!def)(\w|\_)+\([\w\s\d|,]*\)', instructions[i].strip(),
@@ -32,5 +47,5 @@ class InputProgram:
                 if i + 1 != len(instructions):
                     leaders.append(instructions[i+1])
 
-        return list(set(leaders))
+        return leaders
 
