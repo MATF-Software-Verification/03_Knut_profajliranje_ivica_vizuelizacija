8ab0b8d36fef724857751904cf933d8bbf944899
dalma6 <beara.dalma@gmail.com>
2020-04-25 15:57:59 +0200

    add input program indentation check

--- src/main.py
+++ src/main.py
@@ -1,14 +1,20 @@
 import sys
 from src.InputProgram import InputProgram
+from src.checker import check_validity
 
 
 def main():
     if len(sys.argv) != 2:
-        print("Usage: main path/to/file")
+        print('Usage: main path/to/file')
         exit()
 
-    with open(sys.argv[1], 'r') as input:
-        input_program = InputProgram(input.read())
+    if not check_validity(sys.argv[1]):
+        print('Input code not valid')
+        exit()
+
+    with open(sys.argv[1], 'r') as input_file:
+        input_program = InputProgram(input_file.read())
+
         basic_blocks = input_program.divide_into_basic_blocks(input_program.instructions)
         for block in basic_blocks:
             print(block)
