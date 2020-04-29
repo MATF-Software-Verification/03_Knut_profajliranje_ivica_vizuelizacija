0f8e52ecbc83b282c33b7a3204b417d2d0f5647b
dalma6 <beara.dalma@gmail.com>
2020-04-29 19:42:04 +0200

    add start node

--- src/utils/Graph.py
+++ src/utils/Graph.py
@@ -8,11 +8,14 @@ class Graph:
     def create_graph_from_blocks(self, output_text):
         self.graph.add_node('start')
         relations = output_text.split('---------------')
-        for relation in relations:
-            if not '->' in relation:
+        for i in range(len(relations)):
+            if not '->' in relations[i]:
                 continue
 
-            u, v = relation.strip().split('->')
+            u, v = relations[i].strip().split('->')
             self.graph.add_node(u)
+            if 0 == i:
+                self.graph.add_weighted_edges_from([('start', u, 1)])
+
             self.graph.add_node(v)
             self.graph.add_weighted_edges_from([(u, v, 1)])
