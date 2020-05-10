4af5c82a8ce2ecf7a4aa4e9cf005219e636a0948
davgav123 <davidgavrilovic@hotmail.com>
2020-05-11 01:46:32 +0200

    Changes in src/utils/Graph.py
    Added new methods for printing edges and nodes of the graph.

--- src/utils/Graph.py
+++ src/utils/Graph.py
@@ -13,6 +13,9 @@ class Graph:
                 continue
 
             u, v = relations[i].strip().split('->')
+            u = u.strip()
+            v = v.strip()
+            
             self.graph.add_node(u)
             if 0 == i:
                 self.graph.add_weighted_edges_from([('start', u, 1)])
@@ -22,3 +25,12 @@ class Graph:
 
     def create_spanning_tree(self):
         return nx.minimum_spanning_tree(self.graph)
+
+    def edges(self):
+        return nx.edges(self.graph)
+
+    def nodes(self):
+        return nx.nodes(self.graph)
+
+
+
