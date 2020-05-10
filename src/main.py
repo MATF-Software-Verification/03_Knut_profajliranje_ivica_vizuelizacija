9660e38db078a30cdaf0b3a70a1c5c678a2269c5
davgav123 <davidgavrilovic@hotmail.com>
2020-05-11 01:53:16 +0200

    Changes in src/main.py
    Added creation of control flow graph and of spanning tree (of that control flow graph) in main.

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ from src.InputProgram import InputProgram
 from src.checker import check_validity
 from src.instrumentalization.Instrumentalizator import Instrumentalizator
 
+from utils.Graph import Graph
 
 def main():
     if len(sys.argv) != 3:
@@ -23,10 +24,11 @@ def main():
         for block in basic_blocks:
             print(block)
 
+    instrumentalization_out = Path('./instrumentalization_output.txt')
     # instrumentalize source file
     instr = Instrumentalizator(
         Path(sys.argv[1]), Path(sys.argv[2]),
-        './instrumentalization_output.txt'
+        instrumentalization_out
     )
     instr.instrumentalize()
 
@@ -35,6 +37,20 @@ def main():
     # run newly created and instrumentalized, source file
     command = f'python {Path(sys.argv[2])}'
     os.system(command)
+
+    control_flow_graph = Graph()
+    with open(instrumentalization_out, 'r') as instr_out_file:
+        control_flow_graph.create_graph_from_blocks(instr_out_file.read())
+
+    spanning_tree = control_flow_graph.create_spanning_tree()
+
+    print('control flow graph:')
+    print(control_flow_graph.edges())
+    print(control_flow_graph.nodes())
+
+    print('spanning tree:')
+    print(spanning_tree.edges())
+    print(spanning_tree.nodes())
 
 
 if __name__ == '__main__':
