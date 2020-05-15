1d176a0f88b466bc3484f78909b36f9ddeba2e09
davgav123 <davidgavrilovic@hotmail.com>
2020-05-15 21:24:07 +0200

    Changes in src/main.py
    input file changed from text file to python file.
    Deleted everything graph related because of the new instrumentalization.

--- src/main.py
+++ src/main.py
@@ -2,15 +2,12 @@ import sys
 import os
 from pathlib import Path
 
-from src.InputProgram import InputProgram
-from src.checker import check_validity
-from src.instrumentalization.Instrumentalizator import Instrumentalizator
-
-from utils.Graph import Graph
+from InputProgram import InputProgram
+from checker import check_validity
 
 def main():
     if len(sys.argv) != 3:
-        print('Usage: main path/to/input/file.txt path/to/output/file.py', file=sys.stderr)
+        print('Usage: main path/to/input/file.py path/to/output/file.py', file=sys.stderr)
         sys.exit(1)
 
     if not check_validity(Path(sys.argv[1])):
@@ -20,37 +17,11 @@ def main():
     with open(Path(sys.argv[1]), 'r') as input_file:
         input_program = InputProgram(input_file.read())
 
+        block_id = 1
         basic_blocks = input_program.divide_into_basic_blocks(input_program.instructions)
         for block in basic_blocks:
-            print(block)
-
-    instrumentalization_out = Path('./instrumentalization_output.txt')
-    # instrumentalize source file
-    instr = Instrumentalizator(
-        Path(sys.argv[1]), Path(sys.argv[2]),
-        instrumentalization_out
-    )
-    instr.instrumentalize()
-
-    print('Running instrumentalized code...', file=sys.stderr)
-    print('------------------------------', file=sys.stderr)
-    # run newly created and instrumentalized, source file
-    command = f'python {Path(sys.argv[2])}'
-    os.system(command)
-
-    control_flow_graph = Graph()
-    with open(instrumentalization_out, 'r') as instr_out_file:
-        control_flow_graph.create_graph_from_blocks(instr_out_file.read())
-
-    spanning_tree = control_flow_graph.create_spanning_tree()
-
-    print('control flow graph:')
-    print(control_flow_graph.edges())
-    print(control_flow_graph.nodes())
-
-    print('spanning tree:')
-    print(spanning_tree.edges())
-    print(spanning_tree.nodes())
+            print(block.stringify_block(block_id))
+            block_id += 1
 
 
 if __name__ == '__main__':
