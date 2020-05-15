286cf396a209599bf2f10b67e12084fd99fb5df2
davgav123 <davidgavrilovic@hotmail.com>
2020-05-15 21:13:02 +0200

    updated src/BasicBlock.py
    block separator is changed from '-----------' to 'BLOCK id: block_id type: block_type parents: block_parents'.
    This new separator will be used for new instrumentalization.

--- src/BasicBlock.py
+++ src/BasicBlock.py
@@ -15,10 +15,28 @@ class BasicBlock:
     def getLead(self):
         return self.lead
 
-    def __str__(self):
-        str = "-----------\n"
+    def stringify_block(self, block_id):
+        block_type = ''
         for instr in self.instructions:
-            if(instr != ""):
-                str += instr + "\n"
-        str += "-----------"
-        return str
+            if instr.find('if ') != -1:
+                block_type = 'if then'
+                break
+            elif instr.find('else ') != -1:
+                block_type = 'else'
+                break
+            elif instr.find('elif ') != -1:
+                block_type = 'elif'
+                break
+            elif instr.find('for ') != -1:
+                block_type = 'for'
+                break
+            else:
+                block_type = 'ordinary'
+                
+        ret_str = f"# -BEGIN BLOCK id: {block_id} type: {block_type} parents: {'TODO'}\n"
+        for instr in self.instructions:
+            if instr != "":
+                ret_str += instr + "\n"
+        ret_str += f"# -END BLOCK id: {block_id}"
+        
+        return ret_str
